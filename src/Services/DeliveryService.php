32bb64521e3afe6adc3115498049b6110330acf0
Hleb Prakhnitski <thane.crios@gmail.com>
2024-07-30 01:38:04 +0400

    created delivery service

---
+++ src/Services/DeliveryService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Services;
+
+use Entities\Basket;
+use Helpers\PriceHelper;
+
+class DeliveryService
+{
+    use PriceHelper;
+    private Basket $basket;
+
+    const array PRICE_STEPS = [
+        5000 => 495,
+        9000 => 295
+    ];
+
+    public function __construct()
+    {
+        $this->basket = new Basket();
+    }
+
+    final public function calculate(string $basketId, int $withBasketPrice = 1):float|int
+    {
+        $basketPrice = $this->basket->calculateTotalPrice($basketId);
+        var_dump($basketPrice);
+        $deliveryPrice = 0;
+        foreach (self::PRICE_STEPS as $step => $price) {
+            if ($basketPrice < $step) {
+                $deliveryPrice = $price;
+                break;
+            }
+        }
+
+        return $this->normalizePrice(($withBasketPrice ? $basketPrice : 0) + $deliveryPrice);
+    }
+}
